974fd12bcf7ddf604c11cb2d8b728cdf0c55f667
Tercio01 <tercio1parente@gmail.com>
2025-12-06 23:26:16 -0300

    security: Add Helmet.js for HTTP security headers protection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
 import productRoutes from './routes/productRoutes';
@@ -12,6 +13,7 @@ dotenv.config();
 const app: Application = express();
 
 // Middlewares
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
